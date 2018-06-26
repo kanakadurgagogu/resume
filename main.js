51d586798111dddfbfcd99858100ccb93b9d3190
gogu <kanakadurgagogu525@gmail.com>
2018-06-25 14:14:33 -1200

    firstcommit

--- main.js
+++ main.js
@@ -13,9 +13,22 @@ function loadJson(file,callback){
 loadJson("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
+  fun_career(data.careeer);
   fun_education(data.education);
+  fun_achivements(data.achivements);
+  fun_skills(data.skills);
 });
 var div2=document.getElementById("child2");
+function fun_career(car){
+  var c1=document.createElement("h2");
+  c1.textContent="CAREER OBJECTIVE";
+  div2.appendChild(c1);
+  var p=document.createElement("p");
+  p.textContent=car.info;
+  div2.appendChild(p);
+}
+
+
 function fun_education(edu){
    var head=document.createElement("h1");
    head.textContent="EDUCATION QUALIFICATION";
@@ -38,8 +51,30 @@ function fun_education(edu){
 
 
   }
-
-
-
-
+}
+ function fun_achivements(ach){
+   var a1=document.createElement("h2");
+   a1.textContent="ACHIVEMENTS";
+   div2.appendChild(a1);
+   for(var i=0; i<ach.length; i++){
+   var p1=document.createElement("p");
+   p1.innerHTML=ach[i].name;
+   div2.appendChild(p1);
+ }
+}
+function fun_skills(skill){
+  var a1=document.createElement("h2");
+  a1.textContent="SKILLS";
+  div2.appendChild(a1);
+  var hr=document.createElement("hr");
+  a1.appendChild(hr);
+  var tr="";
+  var table=document.createElement("table");
+  table.border="1";
+  div2.appendChild(table);
+  for(var i=0; i<skill.length; i++)
+{
+  tr+="<tr><td>"+skill[i].name+"</td><td>"+skill[i].data+"</td></tr>";
+}
+table.innerHTML=tr;
 }
